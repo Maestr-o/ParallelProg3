dc8075b75e9fabe9cfa72e415a43410e5d81227a
Evgeniy <evg54@internet.ru>
2023-11-06 16:47:05 +0700

    Refactoring, clang-format, corrected linux-errors

--- src/program.cu
+++ src/program.cu
@@ -1,33 +1,39 @@
 ﻿#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include <math.h>
+#include <stdlib.h>
+#include <omp.h>
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-#define CUDA_CHECK_MALLOC if (cudaStatus != cudaSuccess) {\
-  fprintf(stderr, "cudaMalloc failed: %s\n", cudaGetErrorString(cudaStatus));\
-  return;\
-}
+#define CUDA_CHECK_MALLOC                      \
+  if (cudaStatus != cudaSuccess) {             \
+    fprintf(stderr, "cudaMalloc failed: %s\n", \
+            cudaGetErrorString(cudaStatus));   \
+    return;                                    \
+  }
 
-#define CUDA_CHECK_MEMCPY if (cudaStatus != cudaSuccess) {\
-  fprintf(stderr, "cudaMemcpy failed: %s\n", cudaGetErrorString(cudaStatus));\
-  return;\
-}
+#define CUDA_CHECK_MEMCPY                      \
+  if (cudaStatus != cudaSuccess) {             \
+    fprintf(stderr, "cudaMemcpy failed: %s\n", \
+            cudaGetErrorString(cudaStatus));   \
+    return;                                    \
+  }
 
-#define CUDA_CHECK_KERNEL if (cudaStatus != cudaSuccess) {\
-  fprintf(stderr, "Kernel launch failed: %s\n", cudaGetErrorString(cudaStatus));\
-  return;\
-}
+#define CUDA_CHECK_KERNEL                         \
+  if (cudaStatus != cudaSuccess) {                \
+    fprintf(stderr, "Kernel launch failed: %s\n", \
+            cudaGetErrorString(cudaStatus));      \
+    return;                                       \
+  }
 
-void calc(long n_primes, long n);
-bool is_prime(long num);
-long* generate_primes(long N, long* length);
-__global__ void kernel(long* primes, long* size, long* res, long* n);
+void calc(int n_primes, int n);
+bool is_prime(int num);
+int* generate_primes(int N, int* length);
+__global__ void kernel(int* primes, int* size, int* res, int* n);
 
 int main() {
-  long n, n_primes;
+  int n, n_primes;
   printf("Enter N: ");
   if (scanf("%d", &n) != 1) {
     printf("Error\n");
@@ -38,74 +44,79 @@ int main() {
     printf("Error\n");
     return 0;
   }
-  
-  clock_t start, end;
-  start = clock();
+
+  double start, end;
+  start = omp_get_wtime();
   calc(n_primes, n);
-  end = clock();
-  printf("Time: %.3f s", ((double)(end - start)) / CLOCKS_PER_SEC);
+  end = omp_get_wtime();
+  printf("Time: %.3f sec\n", (end - start));
   return 0;
 }
 
-void calc(long n_primes, long n) {
+void calc(int n_primes, int n) {
   cudaDeviceProp prop;
   cudaGetDeviceProperties(&prop, 0);
   cudaError_t cudaStatus;
 
-  long size;
-  long* primes = generate_primes(n_primes, &size);
-  long num_threads = prop.maxThreadsPerBlock;
-  long num_blocks = (size + num_threads - 1) / num_threads;
+  int size;
+  int* primes = generate_primes(n_primes, &size);
+  int num_threads = prop.maxThreadsPerBlock;
+  int num_blocks = (size + num_threads - 1) / num_threads;
 
-  long* dev_primes;
-  long* dev_size;
-  long* dev_res;
-  long* dev_n;
-  long* res = (long*)calloc(size, sizeof(long));
+  int* dev_primes;
+  int* dev_size;
+  int* dev_res;
+  int* dev_n;
+  int* res = (int*)calloc(size, sizeof(int));
   if (res == NULL) {
     printf("Error allocate memory\n");
     return;
   }
 
-  cudaStatus = cudaMalloc((void**)&dev_n, sizeof(long));
+  cudaStatus = cudaMalloc((void**)&dev_n, sizeof(int));
   CUDA_CHECK_MALLOC
-  cudaStatus = cudaMemcpy(dev_n, &n, sizeof(long), cudaMemcpyHostToDevice);
-  CUDA_CHECK_MEMCPY
-  
-  cudaStatus = cudaMalloc((void**)&dev_size, sizeof(long));
-  CUDA_CHECK_MALLOC
-  cudaStatus = cudaMemcpy(dev_size, &size, sizeof(long), cudaMemcpyHostToDevice);
+  cudaStatus = cudaMemcpy(dev_n, &n, sizeof(int), cudaMemcpyHostToDevice);
   CUDA_CHECK_MEMCPY
 
-  cudaStatus = cudaMalloc((void**)&dev_primes, sizeof(long) * size);
+  cudaStatus = cudaMalloc((void**)&dev_size, sizeof(int));
   CUDA_CHECK_MALLOC
-  cudaStatus = cudaMemcpy(dev_primes, primes, sizeof(long) * size,
+  cudaStatus =
+      cudaMemcpy(dev_size, &size, sizeof(int), cudaMemcpyHostToDevice);
+  CUDA_CHECK_MEMCPY
+
+  cudaStatus = cudaMalloc((void**)&dev_primes, sizeof(int) * size);
+  CUDA_CHECK_MALLOC
+  cudaStatus = cudaMemcpy(dev_primes, primes, sizeof(int) * size,
                           cudaMemcpyHostToDevice);
   CUDA_CHECK_MEMCPY
 
-  cudaStatus = cudaMalloc((void**)&dev_res, sizeof(long) * size);
+  cudaStatus = cudaMalloc((void**)&dev_res, sizeof(int) * size);
   CUDA_CHECK_MALLOC
 
   kernel<<<num_blocks, num_threads>>>(dev_primes, dev_size, dev_res, dev_n);
   CUDA_CHECK_KERNEL
 
-  cudaStatus = cudaMemcpy(res, dev_res, sizeof(long) * size,
-                          cudaMemcpyDeviceToHost);
+  cudaStatus =
+      cudaMemcpy(res, dev_res, sizeof(int) * size, cudaMemcpyDeviceToHost);
   CUDA_CHECK_MEMCPY
 
   bool is_answer_found = false;
-  for (long i = 0; i < size; i++) {
+  for (int i = 0; i < size; i++) {
     if (res[i] > n) {
-      printf(
-          "Pair 1: %d, %d (mid: %d)\nPair 2: %d, %d (mid: %d)\nDiff: %d\n", primes[i],
-             primes[i + 1], (primes[i + 1] + primes[i]) / 2, primes[i + 2],
-             primes[i + 3], (primes[i + 3] + primes[i + 2]) / 2, res[i]);
-      is_answer_found = true;
-      break;
+      if (primes[i] > 0 && primes[i + 1] > 0 && primes[i + 2] > 0 && primes[i + 3] > 0) {
+        printf("Pair 1: %d, %d (mid: %d)\nPair 2: %d, %d (mid: %d)\nDiff: %d\n",
+             primes[i], primes[i + 1], (primes[i + 1] + primes[i]) / 2,
+             primes[i + 2], primes[i + 3], (primes[i + 3] + primes[i + 2]) / 2,
+             res[i]);
+        is_answer_found = true;
+        break;
+      } else
+        break;
     }
   }
   if (!is_answer_found)
-    printf("Answer wasn't found. Please increase the maximum number of primes\n");
+    printf(
+        "Answer wasn't found. Please increase the maximum number of primes\n");
 
   free(primes);
   cudaFree(dev_res);
@@ -114,36 +125,34 @@ void calc(long n_primes, long n) {
   cudaFree(dev_n);
 }
 
-__global__ void kernel(long* primes, long* size, long* res, long* n) {
-  long tid = blockIdx.x * blockDim.x + threadIdx.x;
+__global__ void kernel(int* primes, int* size, int* res, int* n) {
+  int tid = blockIdx.x * blockDim.x + threadIdx.x;
   if (tid > *size - 3) return;
-  long diff = (primes[tid + 3] + primes[tid + 2]) / 2 -
-      (primes[tid + 1] + primes[tid]) / 2;
+  int diff = (primes[tid + 3] + primes[tid + 2]) / 2 -
+              (primes[tid + 1] + primes[tid]) / 2;
   if (primes[tid + 3] - primes[tid + 2] == 2 &&
-      primes[tid + 1] - primes[tid] == 2 &&
-      (diff > *n)) {
+      primes[tid + 1] - primes[tid] == 2 && (diff > *n)) {
     res[tid] = diff;
-  }
+  } else
+    res[tid] = 0;
 }
 
-bool is_prime(long num) {
+bool is_prime(int num) {
   if (num < 2) return false;
-  if (num == 2) return true;
-  if (num % 2 == 0) return false;
-  for (long i = 3; i * i <= num; i += 2) {
+  for (int i = 2; i <= sqrt(num); i++) {
     if (num % i == 0) return false;
   }
   return true;
 }
 
-long* generate_primes(long N, long* length) {
-  long* primes = (long*)malloc(sizeof(long));
+int* generate_primes(int N, int* length) {
+  int* primes = (int*)malloc(sizeof(int));
   *length = 0;
 
-  for (long i = 2; i <= N; i++) {
+  for (int i = 2; i <= N; i++) {
     if (is_prime(i)) {
       (*length)++;
-      primes = (long*)realloc(primes, sizeof(long) * (*length));
+      primes = (int*)realloc(primes, sizeof(int) * (*length));
       primes[(*length) - 1] = i;
     }
   }
--- src/seq.c
+++ src/seq.c
@@ -1,8 +1,8 @@
+#include <math.h>
 #include <omp.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 void run();
 bool is_prime(long num);
@@ -10,61 +10,61 @@ bool is_prime_between_pairs(long x, long y);
 void calc(long n);
 
 int main() {
-    run();
-    return 0;
+  run();
+  return 0;
 }
 
 void run() {
-    long n;
-    printf("Enter N: ");
-    scanf("%ld", &n);
-    double start, end;
-    start = omp_get_wtime();
-    calc(n);
-    end = omp_get_wtime();
-    printf("Seq time = %.3lf sec\n", (end - start));
+  long n;
+  printf("Enter N: ");
+  scanf("%ld", &n);
+  double start, end;
+  start = omp_get_wtime();
+  calc(n);
+  end = omp_get_wtime();
+  printf("Time: %.3lf sec\n", (end - start));
 }
 
 void calc(long n) {
-    long last_pair_x = 2, last_pair_y = 3;
-    long prev_prime = 5, current_prime = 7;
-    long middle = 6, last_middle = 2;
-    while (1) {
-        if (is_prime(current_prime)) {
-            if (current_prime - prev_prime == 2) {
-                if (prev_prime != last_pair_y) {
-                    middle = (current_prime + prev_prime) / 2;
-                    if (middle - last_middle > n && !is_prime_between_pairs(last_pair_y, prev_prime)) {
-                        printf("Pair 1: %ld, %ld (mid: %ld)\nPair 2: %ld, %ld (mid: %ld)\n%ld > %ld\n",
-                               last_pair_x, last_pair_y, last_middle, prev_prime, current_prime, middle,
-                               middle - last_middle, n);
-                        break;
-                    }
-                    last_middle = middle;
-                    last_pair_x = prev_prime;
-                    last_pair_y = current_prime;
-                }
-            }
-            prev_prime = current_prime;
+  long last_pair_x = 2, last_pair_y = 3;
+  long prev_prime = 5, current_prime = 7;
+  long middle = 6, last_middle = 2;
+  while (1) {
+    if (is_prime(current_prime)) {
+      if (current_prime - prev_prime == 2) {
+        if (prev_prime != last_pair_y) {
+          middle = (current_prime + prev_prime) / 2;
+          if (middle - last_middle > n &&
+              !is_prime_between_pairs(last_pair_y, prev_prime)) {
+            printf(
+                "Pair 1: %ld, %ld (mid: %ld)\nPair 2: %ld, %ld (mid: "
+                "%ld)\nDiff: %ld\n",
+                last_pair_x, last_pair_y, last_middle, prev_prime,
+                current_prime, middle, middle - last_middle);
+            break;
+          }
+          last_middle = middle;
+          last_pair_x = prev_prime;
+          last_pair_y = current_prime;
         }
-        current_prime++;
+      }
+      prev_prime = current_prime;
     }
+    current_prime++;
+  }
 }
 
 bool is_prime(long num) {
-    if (num < 2)
-        return false;
-    for (long i = 2; i <= sqrt(num); i++) {
-        if (num % i == 0)
-            return false;
-    }
-    return true;
+  if (num < 2) return false;
+  for (long i = 2; i <= sqrt(num); i++) {
+    if (num % i == 0) return false;
+  }
+  return true;
 }
 
 bool is_prime_between_pairs(long x, long y) {
-    for (int i = x + 1; i < y; i++) {
-        if (is_prime(i))
-            return true;
-    }
-    return false;
+  for (int i = x + 1; i < y; i++) {
+    if (is_prime(i)) return true;
+  }
+  return false;
 }
